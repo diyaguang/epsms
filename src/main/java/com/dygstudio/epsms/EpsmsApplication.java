cf3e6c812ef2615ac320c3c7880a55be11c9dd8c
diyaguang <diyaguang@163.com>
2018-02-27 15:00:08 +0800

    Initial commit

---
+++ src/main/java/com/dygstudio/epsms/EpsmsApplication.java
@@ -0,0 +1,12 @@
+package com.dygstudio.epsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsmsApplication.class, args);
+	}
+}
